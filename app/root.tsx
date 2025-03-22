feb9a94ccd59579ff13c00b9841f611aef0b6fe5
Mateo Randulfe <mateorandulfe@gmail.com>
2025-03-22 12:45:45 +0000

    feat: initial DS setup and first component

--- app/root.tsx
+++ app/root.tsx
@@ -11,18 +11,7 @@ import type { Route } from "./+types/root";
 import "./app.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const links: Route.LinksFunction = () => [];
 
 const queryClient = new QueryClient();
 
